cdd1204b0ab269eb13b5119c36a0d0ea6f29a349
Yutaka Murata <p7dea87c7e4n7j@gmail.com>
2020-12-12 02:22:36 +0900

    fix: some missimplementation of UnderdampedLangevin

--- Assets/UnderdampedLangevinManager.cs
+++ Assets/UnderdampedLangevinManager.cs
@@ -5,22 +5,21 @@ using UnityEngine.Assertions;
 public class UnderdampedLangevinManager : MonoBehaviour
 {
     private List<float>      m_NoiseCoefs;
-    private List<float>      m_Gammas;
+    private float[]          m_Gammas;
     private List<Rigidbody>  m_LJRigidbodies;
     private NormalizedRandom m_NormalizedRandom;
 
-    private void Start()
+    private void Awake()
     {
-        m_NormalizedRandom = new NormalizedRandom();
-    }   
+        enabled = false;
+    }
 
     private void FixedUpdate()
     {
-        for (int part_idx = 1; part_idx <= m_LJRigidbodies.Count; part_idx++)
+        for (int part_idx = 0; part_idx < m_LJRigidbodies.Count; part_idx++)
         {
             Rigidbody ljrigid = m_LJRigidbodies[part_idx];
-            Vector3 vel   = ljrigid.velocity;
-            Vector3 accelerate   = -m_Gammas[part_idx] * vel;
+            Vector3 accelerate   = -m_Gammas[part_idx] * ljrigid.velocity;
             Vector3 random_force = new Vector3(m_NormalizedRandom.Generate(),
                                                m_NormalizedRandom.Generate(),
                                                m_NormalizedRandom.Generate());
@@ -30,14 +29,16 @@ public class UnderdampedLangevinManager : MonoBehaviour
     }
 
     internal void Init (float kb, float temperature,
-        List<LennardJonesParticle> ljparticles, List<float> gammas)
+        List<LennardJonesParticle> ljparticles, float[] gammas)
     {
+        enabled = true;
+        m_NormalizedRandom = new NormalizedRandom();
         m_Gammas = gammas;
 
         int particles_num = ljparticles.Count;
         m_LJRigidbodies = new List<Rigidbody>();
         m_NoiseCoefs    = new List<float>();
-        for (int part_idx = 1; part_idx <= particles_num; part_idx++)
+        for (int part_idx = 0; part_idx < particles_num; part_idx++)
         {
             Rigidbody ljrigid = ljparticles[part_idx].GetComponent<Rigidbody>();
             float noise_coef 
@@ -46,7 +47,7 @@ public class UnderdampedLangevinManager : MonoBehaviour
             m_NoiseCoefs.Add(noise_coef);
         }
 
-        Assert.AreEqual(m_LJRigidbodies.Count, m_Gammas.Count,
+        Assert.AreEqual(m_LJRigidbodies.Count, m_Gammas.Length,
             "The number of gamma should equal to that of lennard-jones particles.");
     }
 }
