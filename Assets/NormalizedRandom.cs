49ba87a58cad472f1985f83e72ab12aa48b8931e
Yutaka Murata <p7dea87c7e4n7j@gmail.com>
2020-12-15 00:30:40 +0900

    fix: default arg of NormalizedRandom.Generate

--- Assets/NormalizedRandom.cs
+++ Assets/NormalizedRandom.cs
@@ -14,10 +14,11 @@ public class NormalizedRandom
 
     // Genarate random number from normalized distribution
     // which specified by mean and standard deviation.
-    public float Generate(float mean = 0.0f, float standard_deviation = 0.0f)
+    public float Generate(float mean = 0.0f, float standard_deviation = 1.0f)
     {
         if (!has_stock)
         {
+            // TODO: avoid x and y equal 1.0 or 0.0 case by non adhock method.
             float x = Random.Range(0.01f, 0.99f);
             float y = Random.Range(0.01f, 0.99f);
             float random = Mathf.Sqrt(-2.0f * Mathf.Log(x)) * Mathf.Cos(2.0f * Mathf.PI * y);
