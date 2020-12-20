462cee924eb239df8c103a34e5d51899b981bb58
Yutaka Murata <p7dea87c7e4n7j@gmail.com>
2020-12-20 11:51:47 +0900

    feat: enable to ignore collision between particles bonded

--- Assets/HarmonicBondManager.cs
+++ Assets/HarmonicBondManager.cs
@@ -7,7 +7,7 @@ public class HarmonicBondManager : MonoBehaviour
 {
     private List<float>     m_V0s;
     private List<float>     m_ScaledKs;
-    private List<List<Rigidbody>> m_LJRigidPairs;
+    private List<List<Rigidbody>> m_RigidPairs;
 
     private void Awake()
     {
@@ -17,9 +17,9 @@ public class HarmonicBondManager : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        for (int pair_idx = 0; pair_idx < m_LJRigidPairs.Count; pair_idx++)
+        for (int pair_idx = 0; pair_idx < m_RigidPairs.Count; pair_idx++)
         {
-            List<Rigidbody> ljrigid_pair = m_LJRigidPairs[pair_idx];
+            List<Rigidbody> ljrigid_pair = m_RigidPairs[pair_idx];
             Rigidbody ljrigid_first  = ljrigid_pair[0];
             Rigidbody ljrigid_second = ljrigid_pair[1];
             Vector3 dist_vec = ljrigid_second.position - ljrigid_first.position;
@@ -30,20 +30,28 @@ public class HarmonicBondManager : MonoBehaviour
         }
     }
 
-    internal void Init(List<float> v0s, List<float> ks, List<List<Rigidbody>> ljrigid_pairs, float timescale)
+    internal void Init(List<float> v0s, List<float> ks, List<List<Rigidbody>> rigid_pairs, float timescale)
     {
         enabled = true;
-        Assert.AreEqual(ljrigid_pairs.Count, v0s.Count,
+        Assert.AreEqual(rigid_pairs.Count, v0s.Count,
             "The number of v0 should equal to that of lennard-jones particles.");
-        Assert.AreEqual(ljrigid_pairs.Count, ks.Count,
+        Assert.AreEqual(rigid_pairs.Count, ks.Count,
             "The number of k should equal to that of lennard-jones particles.");
 
         m_V0s = v0s;
-        m_LJRigidPairs = ljrigid_pairs;
+        m_RigidPairs = rigid_pairs;
         m_ScaledKs = new List<float>();
         foreach (float k in ks)
         {
             m_ScaledKs.Add(k * timescale * timescale);
+        }
+
+        // setting ingnore collision
+        foreach (List<Rigidbody> rigid_pair in m_RigidPairs)
+        {
+            Collider first_collider  = rigid_pair[0].GetComponent<Collider>();
+            Collider second_collider = rigid_pair[1].GetComponent<Collider>();
+            Physics.IgnoreCollision(first_collider, second_collider);
         }
     }
 }
