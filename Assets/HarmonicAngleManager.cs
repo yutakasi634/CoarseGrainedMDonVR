bcaf2a2cdbf4dda27573c756df5ba87030b8682e
Yutaka Murata <p7dea87c7e4n7j@gmail.com>
2020-12-24 12:37:00 +0900

    feat: add mock of the class for harmonic bondangle interaction

---
+++ Assets/HarmonicAngleManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarmonicAngleManager : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    internal void Init()
+    {
+        
+    }
+}
