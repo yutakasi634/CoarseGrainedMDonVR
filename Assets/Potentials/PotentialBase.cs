f59c5242ec86e507eb4b9ab2e429e4cbd8eeeec6
Yutaka Murata <p7dea87c7e4n7j@gmail.com>
2021-01-19 22:44:49 +0900

    refactor: add TODO about Rigidbody

--- Assets/Potentials/PotentialBase.cs
+++ Assets/Potentials/PotentialBase.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 internal abstract class PotentialBase
 {
+    //TODO: move Rigidbody list to InteractionManager
     internal List<Rigidbody> m_Rigidbodies;
 
     internal abstract float potential (float r);
