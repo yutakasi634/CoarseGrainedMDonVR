3f67309a0672d366185042b8aa03069edecf9a21
Yutaka Murata <p7dea87c7e4n7j@gmail.com>
2021-01-20 00:30:51 +0900

    fix: derivative of GoContactPotential

--- Assets/Potentials/GoContactPotential.cs
+++ Assets/Potentials/GoContactPotential.cs
@@ -8,13 +8,13 @@ internal class GoContactPotential : PotentialBase
 
     private float m_V0;
     private float m_K;
-    private float m_60ScaledK_V02;
+    private float m_60ScaledK_V0;
 
     internal GoContactPotential(float v0, float k, List<Rigidbody> rigid_bodies, float timescale)
     {
         m_V0            = v0;
         m_K             = k;
-        m_60ScaledK_V02 = 60.0f * k * timescale * timescale / (v0 * v0);
+        m_60ScaledK_V0  = 60.0f * k * timescale * timescale / v0;
         m_Rigidbodies   = rigid_bodies;
     }
 
@@ -33,12 +33,12 @@ internal class GoContactPotential : PotentialBase
     internal override float derivative(float r)
     {
         float invr     = 1.0f / r;
-        float r0invr   = m_V0 * invr;
+        float r0invr   = m_V0     * invr;
         float r0invr2  = r0invr   * r0invr;
         float r0invr4  = r0invr2  * r0invr2;
         float r0invr8  = r0invr4  * r0invr4;
-        float r0invr12 = r0invr8  * r0invr4;
-        float r0invr14 = r0invr12 * r0invr2;
-        return m_60ScaledK_V02 * (r0invr12 - r0invr14) * invr;
+        float r0invr10 = r0invr8  * r0invr2;
+        float r0invr12 = r0invr10 * r0invr2;
+        return m_60ScaledK_V0 * (r0invr10 - r0invr12);
     }
 } 
