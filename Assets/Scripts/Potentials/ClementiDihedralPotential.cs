97963b20ea7e838b2f7101a1b1d8dfdc4a444cd5
Yutaka Murata <p7dea87c7e4n7j@gmail.com>
2021-01-28 12:32:18 +0900

    feat: add mock of DihedralAngleInteraction and potential for that

---
+++ Assets/Scripts/Potentials/ClementiDihedralPotential.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Coral_iMD
+{
+
+    internal class ClementiDihedralPotential : PotentialBase
+    {
+        internal ClementiDihedralPotential()
+        {
+            // TODO: implementation
+        }
+
+        internal override float Potential(float r)
+        {
+            // TODO: implementation
+            return r;
+        }
+
+        internal override float Derivative(float r)
+        {
+            // TODO: implementation
+            return r;
+        }
+
+        internal override string Name()
+        {
+            return "ClementiDihedralPotential";
+        }
+    }
+} // Coral_iMD
