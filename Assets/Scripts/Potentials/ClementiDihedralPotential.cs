63922fce7a6fa99f9c96bed1aca754695dc115c6
Yutaka Murata <p7dea87c7e4n7j@gmail.com>
2021-01-29 13:04:09 +0900

    feat: add DihedralInteractionManager and potential for that

--- Assets/Scripts/Potentials/ClementiDihedralPotential.cs
+++ Assets/Scripts/Potentials/ClementiDihedralPotential.cs
@@ -6,21 +6,25 @@ namespace Coral_iMD
 
     internal class ClementiDihedralPotential : PotentialBase
     {
-        internal ClementiDihedralPotential()
+        private float m_Phi0;
+        private float m_ScaledK;
+
+        internal ClementiDihedralPotential(float phi0, float k, float timescale)
         {
-            // TODO: implementation
+            m_Phi0  = phi0;
+            m_ScaledK = timescale * timescale * k;
         }
 
-        internal override float Potential(float r)
+        internal override float Potential(float phi)
         {
-            // TODO: implementation
-            return r;
+            float phi_diff = phi - m_Phi0;
+            return m_ScaledK * (2.0f - Mathf.Cos(phi_diff) - Mathf.Cos(3.0f * phi_diff));
         }
 
-        internal override float Derivative(float r)
+        internal override float Derivative(float phi)
         {
-            // TODO: implementation
-            return r;
+            float phi_diff = phi - m_Phi0;
+            return 0.5f * m_ScaledK * (Mathf.Sin(2.0f * phi_diff) + 3.0f * Mathf.Sin(6.0f * phi_diff));
         }
 
         internal override string Name()
